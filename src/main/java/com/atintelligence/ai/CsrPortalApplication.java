7718407c01af775545ada67ebe3d557049a65c34
linping huang <huang.lynn@pwc.com>
2016-07-13 17:03:02 +0800

    first commit

---
+++ src/main/java/com/atintelligence/ai/CsrPortalApplication.java
@@ -0,0 +1,18 @@
+package com.atintelligence.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CsrPortalApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CsrPortalApplication.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CsrPortalApplication.class, args);
+	}
+}
---
+++ src/main/java/com/atintelligence/ai/service/spark/testspark.java
@@ -0,0 +1,11 @@
+package com.atintelligence.ai.service.spark;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+/**
+ * Created by lhuang134 on 7/13/2016.
+ */
+public class testspark {
+
+}
